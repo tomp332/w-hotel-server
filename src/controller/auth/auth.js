fde9031715003b74046ee10942c9c16ccf0815c6
tomp332 <tomp12@protonmail.com>
2022-11-12 14:59:36 +0200

    Update auth.js

--- src/controller/auth/auth.js
+++ src/controller/auth/auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const facebookRouter = require("./facebook")
-const { loginValidator } = require("../../middlewears/auth");
+const {loginValidator} = require("../../middlewears/auth");
 const jwt = require("jsonwebtoken");
 const Users = require('../../database/models/users')
+const Hotels = require("../../database/models/hotels");
 
 // Home page route.
-router.post('/login', loginValidator, async function(req, res) {
+router.post('/login', loginValidator, async function (req, res) {
     try {
         const username = req.body.username
         const email = req.body.email
@@ -13,13 +14,15 @@ router.post('/login', loginValidator, async function(req, res) {
             // This can be an email as well
             username: username,
         }
-        let token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h' })
+        let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '24h'})
         const updatedUser = await Users.findOneAndUpdate({
-            $or: [{ username: username }, { email: email }]
-        }, { sessionKey: token }, {})
+            $or: [{username: username}, {email: email}]
+        }, {sessionKey: token}, {})
         console.log(`[+] Authenticated user: ${username || email}`)
         res.cookie('authorization', token)
-        updateUser.isAdmin ? res.render('adminHomePage', { user: updatedUser }) : res.render('home', { user: updatedUser })
+        const hotels = await Hotels.find().exec()
+        updatedUser.isAdmin ? res.render('adminHomePage', {user: updatedUser}) :
+            res.render('home.ejs', {hotels: hotels, user: updatedUser})
     } catch (err) {
         console.log(`Error updating user session key: ${err}`)
         res.sendStatus(500)
