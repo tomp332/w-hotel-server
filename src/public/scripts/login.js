2664559c9e04cb85452a8c70067b6fdf5c5f801e
shir moskovits <shiroshsh980@gmail.com>
2022-11-12 13:49:35 +0200

    fix bugs and add facebook login button

--- src/public/scripts/login.js
+++ src/public/scripts/login.js
@@ -1,4 +1,3 @@
-
 //login/sing up form function
 function setFormMessage(formElement, type, message) {
     const messageElement = formElement.querySelector(".form__message");
@@ -75,21 +74,3 @@ hamburgerMenu.addEventListener('click',function() {
     header.classList.toggle('menu-open');
     
 })
-
-
-
-//google sign up
-// function handleCredentialResponse(response) {
-//     console.log("Encoded JWT ID token: " + response.credential);
-//   }
-//   window.onload = function () {
-//     google.accounts.id.initialize({
-//       client_id: "YOUR_GOOGLE_CLIENT_ID",
-//       callback: handleCredentialResponse
-//     });
-//     google.accounts.id.renderButton(
-//       document.getElementById("buttonDiv"),
-//       { theme: "outline", size: "large" }  // customization attributes
-//     );
-//     google.accounts.id.prompt(); // also display the One Tap dialog
-//   }
