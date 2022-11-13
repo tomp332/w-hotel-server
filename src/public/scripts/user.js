5ea8036f2af032e3e58872fc65c3d4664a7ed6ba
tomp332 <tomp12@protonmail.com>
2022-11-13 19:42:46 +0200

    Fixing java script pages

---
+++ src/public/scripts/user.js
@@ -0,0 +1,36 @@
+
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+window.addEventListener('scroll', function () {
+    let windowPosition = window.scrollY > 0;
+    header.classList.toggle('active', window.scrollY > 0);
+})
+
+hamburgerMenu.addEventListener('click', function () {
+    header.classList.toggle('menu-open');
+
+})
+
+
+
+let logout = document.querySelector('#logout');
+logout.addEventListener('click', async e => {
+    e.preventDefault();
+    await fetch('/auth/logout', {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+    }).then(function (response) {
+        console.log(response)
+        if (response.status === 200 || response.status === 401) {
+            window.location.href = '/'
+        }
+    })
+});
