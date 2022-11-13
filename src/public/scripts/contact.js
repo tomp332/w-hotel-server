b5ceec7b0a2522fe2a737b0a392930c1f7cc0589
tomp332 <tomp12@protonmail.com>
2022-11-13 19:44:24 +0200

    Update contact.js

--- src/public/scripts/contact.js
+++ src/public/scripts/contact.js
@@ -63,3 +63,24 @@ function sendEmail() {
         message => alert(message)
     );
 }
+
+let logout = document.querySelector('#logout');
+logout.addEventListener('click', async e => {
+    e.preventDefault();
+    await fetch('/auth/logout', {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+    }).then(function (response) {
+        console.log(response)
+        if (response.status === 200 || response.status === 401) {
+            window.location.href = '/'
+        }
+    })
+});
