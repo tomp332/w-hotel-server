79dbcbe026520ce796d603b2bf67e70f21bed8e7
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2022-11-19 20:36:27 +0200

    changes

--- src/public/scripts/admin.js
+++ src/public/scripts/admin.js
@@ -187,28 +187,6 @@ async function save_row(btnNum, reservationId)
 }
 
 
-function searchFunc(){
-    // Declare variables
-    let input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("admin__search");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("reservations-table");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-
 async function delete_hotel_row(btnNum) {
     let rowId = btnNum.split('-')[1]
     return await fetch('/api/hotels', {
